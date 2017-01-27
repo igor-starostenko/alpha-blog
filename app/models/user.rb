daddc942c5a25091df8f86b9b5488d166549beb2
igor-starostenko <contactigorstar@gmail.com>
2017-01-26 22:30:02 -0800

    username and email validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class Article < ActiveRecord::Base
-
+  validates :username,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { minimum: 3, maximum: 20 }
+  VALID_EMAIL_REGEX = /   /i
+  validates :email,
+            presence: true,
+            uniqueness: { case_sensitive: false },
+            length: { maximum: 105 },
+            format: { with: VALID_EMAIL_REGEX}
 end
