9558f034689da79bdd3d3118ebcd372618ba7833
igor-starostenko <contactigorstar@gmail.com>
2017-02-11 18:07:13 -0800

    authentication dependent validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = 'Your account has been successfully created!'
-      redirect_to articles_path
+      redirect_to user_path(current_user.id)
     else
       # render plain: params[:user].inspect
       render 'new'
