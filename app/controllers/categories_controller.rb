c37aa6426f4da3311b33b267c714b8e750f3008d
igor-starostenko <contactigorstar@gmail.com>
2017-02-22 22:35:38 -0800

    pagination for categories styled

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,7 @@ class CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :edit, :update, :destroy]
 
   def index
-    @categories = Category.paginate(page: params[:page], per_page: 10)
+    @categories = Category.paginate(page: params[:page], per_page: 8)
   end
 
   def new
