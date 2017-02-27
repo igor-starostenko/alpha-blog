26b995585ab4351c5899199576881ec26e2e548a
igor-starostenko <contactigorstar@gmail.com>
2017-02-27 00:01:31 -0800

    tests to verify edit and delete links on category listing

--- test/integration/list_categories_test.rb
+++ test/integration/list_categories_test.rb
@@ -14,6 +14,18 @@ class ListCategoriesTest < ActionDispatch::IntegrationTest
     assert_select 'div', text: @category2.name
   end
 
+  test 'should show "Edit" link next to a category' do
+    get categories_path
+    assert_select 'a[href=?]', edit_category_path(@category.id), text: 'Edit'
+    assert_select 'a[href=?]', edit_category_path(@category2.id), text: 'Edit'
+  end
+
+  test 'should show "Delete" link next to a category' do
+    get categories_path
+    assert_select 'a[href=?]', category_path(@category.id), text: 'Delete', method: :delete
+    assert_select 'a[href=?]', category_path(@category2.id), text: 'Delete', method: :delete
+  end
+
   test 'should have a link to create new category' do
     get categories_path
     assert_select 'a[href=?]', new_category_path, text: 'New Category'
